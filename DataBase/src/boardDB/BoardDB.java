81c462efc57eec8fffe012dc446dd9c5ba8e99ab
NTL <58549685+brad0520@users.noreply.github.com>
2021-03-27 13:08:01 +0900

    Update BoardDB.java

--- DataBase/src/boardDB/BoardDB.java
+++ DataBase/src/boardDB/BoardDB.java
@@ -12,7 +12,7 @@ public class BoardDB {
 	public static void main(String[] args) throws ClassNotFoundException, SQLException {
 		Scanner sc = new Scanner(System.in);
 
-		// 데이터베이스 연결
+		// �뜲�씠�꽣踰좎씠�뒪 �뿰寃�
 		String driver = "com.mysql.cj.jdbc.Driver";
 		String url = "jdbc:mysql://localhost:3306/BoardDB?serverTimezone=UTC";
 		String user = "root";
@@ -20,15 +20,15 @@ public class BoardDB {
 		Class.forName(driver); 
 		Connection conn = DriverManager.getConnection(url, user, pass); 
 
-		// BoardDB 기능 구현
+		// BoardDB 湲곕뒫 援ы쁽
 		while (true) {
-			System.out.print("명령어를 입력해주세요 : ");
+			System.out.print("紐낅졊�뼱瑜� �엯�젰�빐二쇱꽭�슂 : ");
 			String cmd = sc.nextLine();
 
 			if (cmd.equals("list")) {
 
 				String sql = "SELECT * FROM article";
-				// 조회한 데이터를 rs에 저장
+				// 議고쉶�븳 �뜲�씠�꽣瑜� rs�뿉 ���옣
 				PreparedStatement pstmt = conn.prepareStatement(sql); 
 				ResultSet rs = pstmt.executeQuery(); 
 
@@ -45,38 +45,38 @@ public class BoardDB {
 				}
 			} else if (cmd.equals("add")) {
 
-				System.out.print("제목 : ");
+				System.out.print("�젣紐� : ");
 				String title = sc.nextLine();
-				System.out.print("내용 : ");
+				System.out.print("�궡�슜 : ");
 				String body = sc.nextLine();
 				String regDate = Util.getCurrentDate();
 
-				String sql = "INSERT INTO article SET title = ?, `body` = ?, writer = '홍길동', regDate = ?, hit = 10";
+				String sql = "INSERT INTO article SET title = ?, `body` = ?, writer = '�솉湲몃룞', regDate = ?, hit = 10";
 				PreparedStatement pstmt = conn.prepareStatement(sql);
 
 				pstmt.setString(1, title);
 				pstmt.setString(2, body);
 				pstmt.setString(3, regDate);
 				
-				// 추가된 데이터를 데이터베이스에 저장 실행
+				// 異붽��맂 �뜲�씠�꽣瑜� �뜲�씠�꽣踰좎씠�뒪�뿉 ���옣 �떎�뻾
 				pstmt.executeUpdate();
 
 			} else if (cmd.equals("update")) {
-				System.out.print("번호 : ");
+				System.out.print("踰덊샇 : ");
 				int id = Integer.parseInt(sc.nextLine());
 
 				String sql = "select * from article where id = ?";
 				PreparedStatement pstmt = conn.prepareStatement(sql);
 
 				pstmt.setInt(1, id);
-				// 요청된 데이터를 rs에 저장
+				// �슂泥��맂 �뜲�씠�꽣瑜� rs�뿉 ���옣
 				ResultSet rs = pstmt.executeQuery();
 
 				if (rs.next()) {
-					// 게시물 번호가 있는 경우 실행
-					System.out.print("제목 : ");
+					// 寃뚯떆臾� 踰덊샇媛� �엳�뒗 寃쎌슦 �떎�뻾
+					System.out.print("�젣紐� : ");
 					String title = sc.nextLine();
-					System.out.print("내용 : ");
+					System.out.print("�궡�슜 : ");
 					String body = sc.nextLine();
 
 					String sql2 = "UPDATE article SET title = ?, `body` = ? WHERE id = ?";
@@ -85,15 +85,15 @@ public class BoardDB {
 					pstmt2.setString(1, title);
 					pstmt2.setString(2, body);
 					pstmt2.setInt(3, id);
-					// 변경된 데이터를 데이터베이스에 업데이트
+					// 蹂�寃쎈맂 �뜲�씠�꽣瑜� �뜲�씠�꽣踰좎씠�뒪�뿉 �뾽�뜲�씠�듃
 					pstmt2.executeUpdate();
 
-				} else { // 게시물 번호가 있는 경우 실행 
-					System.out.println("없는 게시물입니다.");
+				} else { // 寃뚯떆臾� 踰덊샇媛� �엳�뒗 寃쎌슦 �떎�뻾 
+					System.out.println("�뾾�뒗 寃뚯떆臾쇱엯�땲�떎.");
 
 				}
 			} else if (cmd.equals("delete")) {
-				System.out.print("번호 : ");
+				System.out.print("踰덊샇 : ");
 				int id = Integer.parseInt(sc.nextLine());
 
 				String sql = "select * from article where id = ?";
@@ -108,15 +108,15 @@ public class BoardDB {
 
 					pstmt2.setInt(1, id);
 
-					// 데이터 삭제 실행
+					// �뜲�씠�꽣 �궘�젣 �떎�뻾
 					pstmt2.executeUpdate();
 
 				} else {
-					System.out.println("없는 게시물입니다.");
+					System.out.println("�뾾�뒗 寃뚯떆臾쇱엯�땲�떎.");
 				}
 			}
 			else if(cmd.equals("read")){
-				System.out.print("번호 : ");
+				System.out.print("踰덊샇 : ");
 				int id = Integer.parseInt(sc.nextLine());
 
 				String sql = "select * from article where id = ?";
@@ -133,15 +133,15 @@ public class BoardDB {
 					String regDate = rs.getString("regDate");
 					String hit = rs.getString("hit");
 
-					System.out.println("번호 : " + id2);
-					System.out.println("제목 : " + title);
-					System.out.println("내용 : " + body);
-					System.out.println("작성자 : " + writer);
-					System.out.println("등록날짜 : " + regDate);
-					System.out.println("조회수 : " + hit);
+					System.out.println("踰덊샇 : " + id2);
+					System.out.println("�젣紐� : " + title);
+					System.out.println("�궡�슜 : " + body);
+					System.out.println("�옉�꽦�옄 : " + writer);
+					System.out.println("�벑濡앸궇吏� : " + regDate);
+					System.out.println("議고쉶�닔 : " + hit);
 
 				} else {
-					System.out.println("없는 게시물입니다.");
+					System.out.println("�뾾�뒗 寃뚯떆臾쇱엯�땲�떎.");
 				}
 			}
 		}
